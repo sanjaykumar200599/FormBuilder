e330fa5fe5d8d692db5302b441e2cca5d051aa09
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-08-08 00:19:31 +0530

    Modified the form api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-// import FillFormPage from "./pages/FillFormPage";
-// import Editor from "./pages/Editor";
+import Editor from "./pages/Editor";
+import FillFormPage from "./pages/FillFormPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Editor />} />
-        <Route path="/form/:formId" element={<FillFormPage />} /> */}
+        <Route path="/" element={<Editor />} />
+        <Route path="/form/:formId" element={<FillFormPage />} />
       </Routes>
     </Router>
   );
--- client/src/pages/Editor.tsx
+++ client/src/pages/Editor.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import {
   Question,
-  QuestionType,
+  //QuestionType,
   CategorizeQuestion,
   ClozeQuestion,
   ComprehensionQuestion,
